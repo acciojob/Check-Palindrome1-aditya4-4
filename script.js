6a78e760e0d9ca6beb585d823a07e4b9aca76d36
DURGESH KUMAR <68071906+aditya4-4@users.noreply.github.com>
2023-04-23 11:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(s){
+const str = s.split(" ");
+let mystr = str.join("");
+mystr = mystr.toLowerCase();
 
+	 let start =0;
+    let end =mystr.length -1;
+    while(start < end){
+        if(mystr[start] != mystr[end]){
+            return false
+        }
+        start++;
+        end--;
+    }
+    return true;
 }
 module.exports = palindrome
